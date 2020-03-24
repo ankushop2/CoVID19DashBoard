027eb51c466ae1b93bdf442929e4002bb048624c
Ankush Arunkumar <ankush1997@gmail.com>
2020-03-24 19:06:00 +0530

    First Commit

---
+++ regress.py
@@ -0,0 +1,89 @@
+import requests
+from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
+import operator
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
+def train_model(x, y, polynomial_degree):
+    polynomial_features = PolynomialFeatures(degree=polynomial_degree)
+    x_poly = polynomial_features.fit_transform(x)
+
+    model = LinearRegression()
+    model.fit(x_poly, y)
+
+    return model
+
+def get_predictions(x, model, polynomial_degree):
+    polynomial_features = PolynomialFeatures(degree=polynomial_degree)
+    x_poly = polynomial_features.fit_transform(x)
+
+    return model.predict(x_poly)
+
+def print_stats( model, x, y, polynomial_degree, days_to_predict):
+    y_pred = np.round(get_predictions(x, model, polynomial_degree), 0).astype(np.int32)
+    days_pred = print_forecast(model, polynomial_degree, beginning_day=len(x), limit=days_to_predict)
+    actual_poly = print_model_polynomial(model)
+
+    return y_pred, days_pred, actual_poly
+
+    #plot_graph(model_name, x, y, y_pred)
+
+def print_model_polynomial( model):
+    coef = model.coef_
+    intercept = model.intercept_
+    poly = "{0:.3f}".format(intercept)
+
+    for i in range(1, len(coef)):
+        if coef[i] >= 0:
+            poly += " + "
+        else:
+            poly += " - "
+        
+        poly += "{0:.5f}".format(coef[i]).replace("-", "") + "X^" + str(i)
+
+    return poly
+   #print("The " + model_name + " model function is: f(X) = " + poly)
+
+def plot_graph( x, y, y_pred):
+    plt.scatter(x, y, s=10)
+    sort_axis = operator.itemgetter(0)
+    sorted_zip = sorted(zip(x, y_pred), key=sort_axis)
+    x, y_pred = zip(*sorted_zip)
+    
+    plt.plot(x, y_pred, color='m')
+    plt.title("Amount of " + model_name + " in each day")
+    plt.xlabel("Day")
+    plt.ylabel(model_name)
+    plt.show()
+
+def print_forecast(model, polynomial_degree, beginning_day=0, limit=10):
+    next_days_x = np.array(range(beginning_day, beginning_day + limit)).reshape(-1, 1)
+    next_days_pred = np.round(get_predictions(next_days_x, model, polynomial_degree), 0).astype(np.int32)
+    return next_days_pred;
+
+    # print("The forecast for " + model_name + " in the following " + str(limit) + " days is:")
+
+    # for i in range(0, limit):
+    #     print(str(i + 1) + ": " + str(next_days_pred[i]))
+
+
+
+def final():
+    URL = "https://pomber.github.io/covid19/timeseries.json"
+    r = requests.get(URL)
+    data = r.json()
+    india = data["India"]
+    india_data = [d.get("confirmed") for d in india]
+    x = []
+    for i in range(0, len(india_data)):
+        x.append(i)
+    
+    x = np.array(x).reshape(-1, 1)
+    y = np.array(india_data)
+    model = train_model(x, y,5)
+    y_pred, days_pred, actual_poly = (print_stats(model, x, y, 5, 5))
+    return x, y, y_pred, days_pred, actual_poly
+
---
+++ scrape.py
@@ -0,0 +1,128 @@
+import requests
+import re 
+import os.path
+from os import path
+import bs4
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+states = {}
+states["Andhra Pradesh"]="AP"
+states["Arunachal Pradesh"]="AR"
+states["Assam"]="AS"
+states["Bihar"]="BR"
+states["Chattisgarh"]="CT"
+states["Chhattisgarh"]="CT"
+states["Goa"]="GA"
+states["Gujarat"]="GJ"
+states["Haryana"]="HR"
+states["Himachal Pradesh"]="HP"
+states["Jharkhand"]="JH"
+states["Karnataka"]="KA"
+states["Kerala"]="KL"
+states["Madhya Pradesh"]="MP"
+states["Maharashtra"]="MH"
+states["Manipur"]="MN"
+states["Meghalaya"]="ML"
+states["Mizoram"]="MZ"
+states["Nagaland"]="NL"
+states["Odisha"]="OR"
+states["Punjab"]="PB"
+states["Rajasthan"]="RJ"
+states["Sikkim"]="SK"
+states["Tamil Nadu"]="TN"
+states["Telengana"]="TG"
+states["Tripura"]="TR"
+states["Uttarakhand"]="UT"
+states["Uttar Pradesh"]="UP"
+states["West Bengal"]="WB"
+states["Andaman and Nicobar Islands"]="AN"
+states["Chandigarh"]="CH"
+states["Dadra and Nagar Haveli"]="DN"
+states["Daman and Diu"]="DD"
+states["Delhi"]="DL"
+states["Jammu and Kashmir"]="JK"
+states["Ladakh"]="LA"
+states["Lakshadweep"]="LD"
+states["Pondicherry"]="PY"
+states["Puducherry"]="PY"
+
+
+
+def getContents():
+	url = "https://www.mohfw.gov.in/"
+	r = requests.get(url)	
+	txt = ""
+	if r.status_code == 200:
+		txt = r.text
+		return txt
+
+def scrape_now():
+	txt = getContents()
+	actual_data = {}
+	state_list=[]
+	confirmed_data = []
+	cured_data = []
+	death_data = []
+	confirmed_india = 0
+	confirmed_foreign = 0
+	confirmed_cured = 0
+	confirmed_deaths = 0
+	#get to parsing
+	soup = BeautifulSoup(txt, 'html.parser')
+	cases = soup.find(id="cases")
+	tables = cases.find_all("tbody")
+	
+	if len(tables) > 0:
+		table = tables[0]
+		#print(table)
+		first_row = False
+		for tr in list(table.children):
+			if isinstance(tr, bs4.element.Tag): 
+				if first_row:
+					print("FIRST ROW")
+					first_row = False
+					continue
+
+				tds = list(tr.children)
+
+				if "Total number of confirmed cases in India" == tds[1].get_text():
+					continue
+
+				now = datetime.now()
+				report_time = now.strftime("%d/%m/%Y %H:%M:%S")
+				state = (tds[3]).get_text()
+				state = state.replace("Union Territory of ","")
+				state = state.strip()
+				state_code = ""
+				if state in states :
+					state_code = (states[state]).lower()
+				else:
+					sendMessage("ERROR WRONG STATE",  "Not found: {0}".format(state))
+					print("------> Wrong state {0}".format(state))
+					return 0
+
+				
+				data = {}
+				#data["state"] = state_code
+				data["report_time"] = report_time
+				confirmed_india = confirmed_india + int( (tds[5]).get_text() )
+				data["confirmed_india"] =  int( (tds[5]).get_text() )
+				confirmed_foreign = confirmed_foreign + int( (tds[7]).get_text() )
+				data["confirmed_foreign"] = int( (tds[7]).get_text() )
+				state_list.append(state)
+				confirmed_data.append(data["confirmed_india"]+data["confirmed_foreign"])
+				confirmed_cured = confirmed_cured + int( (tds[9]).get_text() )
+				data["cured"] = int( (tds[9]).get_text() )
+				cured_data.append(int( (tds[9]).get_text() ))
+				confirmed_deaths = confirmed_deaths + int( (tds[11]).get_text() )
+				data["death"] = int( (tds[11]).get_text() )
+				death_data.append(int( (tds[11]).get_text() ))
+				actual_data[state] = data
+
+		return state_list, confirmed_data, cured_data, death_data, actual_data, confirmed_india, confirmed_foreign, confirmed_cured, confirmed_deaths
+		
+		
+	else:
+		return "!!!!ERROR!!!!"
+
